aa70190bef6dad8df202727ebf6b10c612017798
arthurgmt <arthur.guillaumot@gmail.com>
2022-11-22 09:25:04 +0100

    gitignore

--- src/Dessin.java
+++ src/Dessin.java
@@ -4,8 +4,10 @@ public class Dessin implements Transformation{
 
     ArrayList<Image> images;
 
+    public Dessin(){
+        this.images = new ArrayList<Image>();
+    }
 
-    
     @Override
     public void homothetie() {
         // TODO Auto-generated method stub
