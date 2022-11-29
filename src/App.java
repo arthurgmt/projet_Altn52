61919b10cccf670f8cd375de771bd663dc8ca118
arthurgmt <arthur.guillaumot@gmail.com>
2022-11-29 17:11:16 +0100

    udpate App

--- src/App.java
+++ src/App.java
@@ -2,39 +2,36 @@ import java.util.ArrayList;
 
 public class App{
 
-    public static void main(String[] args){
+  public static void main(String[] args){
 
             
 		Dessin pan = new Dessin();
 
-        Image i = new Image();
+    Image i = new Image();
+    Forme r_ = new Rectangle(0, 0, 500, 500);
+    i.addForme(r_);
 
-        Forme e = new Elipse(50,50,100,150);
-        Forme c = new Elipse(100,100,100,100);
-        Forme t = new Trait(0, 0, 500, 500);
-        Forme r = new Rectangle(20, 50, 400, 200);
+    // Forme e = new Elipse(50,50,100,150);
+    Forme c = new Elipse(0,0,100,100);
+    // Forme t = new Trait(200, 200, 500, 500);
+    Forme r = new Rectangle(0, 0, 100, 80);
 
-        Forme p = new Polygone();
-        p.addPoint(200,40);
-        p.addPoint(250,100);
-        p.addPoint(180,200);
+    // add formes to image
+    // i.addForme(e);
+    // i.addForme(t);
+    // i.addForme(c);
+    i.addForme(r);
 
-        // add formes to image
-        i.addForme(e);
-        i.addForme(t);
-        i.addForme(c);
-        i.addForme(r);
-        i.addForme(p);
+    // add image 
+    pan.addImage(i);
 
-        // add image 
-        pan.addImage(i);
+    // view
+    Frame fen = new Frame(500,520);
 
-        // view
-        Frame fen = new Frame(500,500);
 		fen.add(pan);
 		fen.repaint();
 		fen.revalidate();
 
-    }
+  }
 
 }
